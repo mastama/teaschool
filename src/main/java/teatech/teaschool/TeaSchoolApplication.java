ce46e9715f1740690d97d43914e375911f427c0a
Singgih Pratama <singgihpratama96@gmail.com>
2023-08-15 21:10:37 +0700

    initialize project TeaSchool

---
+++ src/main/java/teatech/teaschool/TeaSchoolApplication.java
@@ -0,0 +1,13 @@
+package teatech.teaschool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeaSchoolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TeaSchoolApplication.class, args);
+    }
+
+}
---
+++ src/test/java/teatech/teaschool/TeaSchoolApplicationTests.java
@@ -0,0 +1,13 @@
+package teatech.teaschool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeaSchoolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
